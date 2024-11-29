33475f1f9d0c8f0baa8dc22307aac46a68fcfa9d
AsmaSendjakeddie <a_sendjakeddine@estin.dz>
2024-11-29 15:14:01 +0100

    Navbar modified

--- Frontend/src/routes/Routers.jsx
+++ Frontend/src/routes/Routers.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import Signin from "../pages/Signin";
-
+import { Navbar } from "../components/Navbar/Navbar";
 
 const Routers = () => {
   const { pathname } = useLocation();
@@ -14,6 +14,7 @@ const Routers = () => {
   return (
     <Routes>
       <Route path="/si" element={<Signin />} />
+      <Route path="/navbar" element={<Navbar />} />
     </Routes>
   );
 };
