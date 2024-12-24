536906f92acf9ccddc2bf6fd9ffdeb4a4ba3b61c
asmaBelkerrouche <164345614+asmaBelkerrouche@users.noreply.github.com>
2024-12-24 18:31:09 +0100

    12/24/2024

--- Backend/pp.js
+++ Backend/pp.js
@@ -1,50 +1,51 @@
 const express = require('express');
-const app = express();
+const cors = require("cors");
 const multer = require('multer');
 const path = require('path');
+const bodyParser = require("body-parser");
 const userRoutes = require('./route');
-const bodyParser = require('body-parser');
+const app = express();
+
+
+app.use(cors());
+const upload =  require('./route');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser()); 
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('token');  // Clears the token cookie
+  res.status(200).json({ message: 'Logged out successfully' });
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, './uploads/');
   },
   filename: (req, file, cb) => {
-    const fileExtension = path.extname(file.originalname);
-    const filename = Date.now() + fileExtension;
-    cb(null, filename);
-  }
+    cb(null, Date.now() + path.extname(file.originalname)); }
 });
 
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 },
-  fileFilter: (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-    if (!allowedTypes.includes(file.mimetype)) {
-      return cb(new Error('Invalid file type'));
-    }
-    cb(null, true);
-  }
-});
 
-app.post('/upload', upload.array('photos', 10), (req, res) => {
-  if (!req.files || req.files.length === 0) {
-    return res.status(400).json({ error: 'No photos uploaded' });
-  }
+// app.post('/upload', upload.array('photos', 10), (req, res) => {
+//   if (!req.files || req.files.length === 0) {
+//     return res.status(400).json({ error: 'No photos uploaded' });
+//   }
 
-  const photoUrls = req.files.map(file => `/uploads/${file.filename}`);
-  res.status(200).json({ message: 'Files uploaded successfully', photoUrls });
-});
+//   const photoUrls = req.files.map(file => `/uploads/${file.filename}`);
+//   res.status(200).json({ message: 'Files uploaded successfully', photoUrls });
+// });
 
-app.use(express.json());
-app.use(bodyParser.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/comments', userRoutes);
 app.use('/api/property', userRoutes);
 app.use('/api/history', userRoutes);
 app.use('/api/messeges', userRoutes);
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
