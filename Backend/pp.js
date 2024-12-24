70d8f004dbeb75e686bbd715992005dacdebea00
asmaBelkerrouche <164345614+asmaBelkerrouche@users.noreply.github.com>
2024-12-24 23:23:02 +0100

    12/24/2024

--- Backend/pp.js
+++ Backend/pp.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const userRoutes = require('./route');
 const app = express();
 
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+const passport = require('passport');
 
 app.use(cors());
 const upload =  require('./route');
@@ -15,6 +18,71 @@ const cookieParser = require('cookie-parser')
 app.use(cookieParser()); 
 app.use(express.json());
 app.use(bodyParser.json());
+
+const GOOGLE_CLIENT_ID = "408478996991-prag7l93m9v845ms2ft7oighd3o17edh.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = " 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";
+
+app.use(session({
+  secret: 'd1565e2de497088d875c5b66f985b5245f559212a48773f1f6781ff76d501760f9558a3a5f8cee8d118b0abc9d35bd046f41579b2cfc79d420370d9ae570b26f',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+app.get("/auth/google", passport.authenticate("google", { 
+  scope: ["profile", "email"] 
+}));
+
+app.get("/auth/google/callback", 
+  passport.authenticate("google", { failureRedirect: "/auth/failed" }),
+  (req, res) => {
+      res.json({ 
+          message: "Successfully logged in",
+          user: req.user 
+      });
+  }
+);
+
+app.get("/auth/failed", (req, res) => {
+  res.status(401).json({
+      message: "Authentication failed"
+  });
+});
+app.get("/profile_google", (req, res) => {
+  if (req.isAuthenticated()) {
+      res.json({
+          message: "Profile data",
+          user: req.user
+      });
+  } else {
+      res.status(401).json({
+          message: "Not authenticated"
+      });
+  }
+});
+
+app.get("/logout_google", (req, res) => {
+  req.logout((err) => {
+      if (err) {
+          return res.status(500).json({ message: "Error logging out" });
+      }
+      res.json({ message: "Successfully logged out" });
+  });
+});
 
 app.post('/logout', (req, res) => {
   res.clearCookie('token');  // Clears the token cookie
