0d5b9f48b3bc22f34cde683f51d62f0808f2e345
AsmaSendjakeddie <a_sendjakeddine@estin.dz>
2025-01-26 03:04:50 +0100

    some changes

--- Backend/route.js
+++ Backend/route.js
@@ -1,34 +1,60 @@
-const express = require('express');
-const { signup, login,getMessages,getit , logout, getPropertyDetails ,getLikedProperties,getSavedProperties,getThreeRandomProperties,searchProperties,  createMessage,saveProperty, his ,addLike ,authenticate,  deleteProperty ,addComment, cc, zz ,getRandomProperties } = require('./views');
+const express = require("express");
+const {
+  signup,
+  login,
+  getget,
+  getMessages,
+  getit,
+  createMessage,
+  createeessage,
+  getMessases,
+  getLikedProperties,
+  getSavedProperties,
+  getThreeRandomProperties,
+  searchProperties,
+  logout,
+  saveProperty,
+  creaeeMessage,
+  getPropertyDetails,
+  his,
+  addLike,
+  authenticate,
+  deleteProperty,
+  addComment,
+  cc,
+  zz,
+  getRandomProperties,
+} = require("./views");
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
-
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/:user_id/comment', addComment);
-router.post('/logout', logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/:user_id/comment", addComment);
+router.post("/logout", logout);
 //_________________________________________
-router.get('/random', getRandomProperties);
-router.get('/randomp', getRandomProperties);
-router.get('/randomc', getRandomProperties);
+router.get("/random", getRandomProperties);
+router.get("/randomp", getRandomProperties);
+router.get("/randomc", getRandomProperties);
 // _______________________________________________________
 
-router.delete('/:id/deleteit', deleteProperty);
-router.post('/like', addLike);
-router.post('/save', saveProperty);
-router.post('/userhis' , his);
-router.post('/createMessage', createMessage); 
-router.get('/home', getit);
-router.get('/searchProperties', searchProperties);
-router.get('/getThreeRandomProperties', getThreeRandomProperties);
-router.get('/:user_id/getLikedProperties', getLikedProperties);
-router.get('/getPropertyDetails/:property_id', getPropertyDetails);
-router.get('/:user_id/getSavedProperties', getSavedProperties);
+router.delete("/:id/deleteit", deleteProperty);
+router.post("/like", addLike);
+router.post("/save", saveProperty);
+router.post("/userhis", his);
+router.post("/createMessage", createMessage);
+router.get("/home", getit);
+router.get("/searchProperties", searchProperties);
+router.get("/getThreeRandomProperties", getThreeRandomProperties);
+router.get("/:user_id/getLikedProperties", getLikedProperties);
+router.get("/getPropertyDetails/:property_id", getPropertyDetails);
+router.get("/:user_id/getSavedProperties", getSavedProperties);
+router.post("/cmassage", creaeeMessage);
 
-
+// Fetch messages between two users
+router.get("/getget ", getget);
 
 // ____________________________________________
-router.get('/getMessages', getMessages);  
+// router.get("/getMessages", getMessases);
 module.exports = router;
